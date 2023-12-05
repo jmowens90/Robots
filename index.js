c883e464426d58e713f25214267ad913929c62d9
Jaylen <jmowens90@gmail.com>
2023-12-05 16:34:26 -0500

    added server capabilites

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const client = require("./db/client.cjs");
+client.connect();
+
+app.get("/", (req, res, next) => {
+  res.send("TESTING!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
