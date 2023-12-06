67c1e2643272a48396ad35590f4fe2cdfa78aed5
Jaylen <jmowens90@gmail.com>
2023-12-06 10:25:42 -0500

    minor changes

--- db/client.cjs
+++ db/client.cjs
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/robots';
+const connectionString = process.env.DATABASE_URL || "dbaddress";
 
 const client = new Client({
     connectionString,
